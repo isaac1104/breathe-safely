ad96556a9d18409c1bcc6ce9035ac1f6c51cf775
isaac1104 <isaac-kwon@hotmail.com>
2019-06-30 17:56:07 +0900

    create Home component

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,13 +1,13 @@
 import React, { Component } from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
-import Landing from './Landing/Landing';
+import Home from './Home/Home';
 
 class App extends Component {
   render() {
     return (
       <BrowserRouter>
         <Switch>
-          <Route exact path='/' component={Landing} />
+          <Route exact path='/' component={Home} />
         </Switch>
       </BrowserRouter>
     );
---
+++ client/src/components/Home/Home.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import styles from './Home.module.css';
+
+const Home = () => (
+  <div className={styles.HomeContainer}>
+    <h1>Home Route</h1>
+  </div>
+);
+
+export default Home;
