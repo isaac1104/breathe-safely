041b7c13b084ab4c7e77c67bdebfcd4c7225329e
isaac1104 <isaac-kwon@hotmail.com>
2019-06-30 21:44:24 +0900

    show loading spinner while data is fetching

--- client/src/components/Home/Home.js
+++ client/src/components/Home/Home.js
@@ -1,20 +1,36 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { Typography } from 'antd';
+import { Typography, Spin, Icon } from 'antd';
 import SearchInput from '../SearchInput/SearchInput';
 import styles from './Home.module.css';
 
 const { Title } = Typography;
 
 class Home extends Component {
+  renderLoading() {
+    const { is_fetching } = this.props.air_data;
+    if (is_fetching) {
+      return (
+        <Spin indicator={
+          <Icon
+            type='loading'
+            className={styles.SpinnerIcon}
+          />
+        }
+        />
+      );
+    }
+    return <SearchInput />;
+  }
+
   render() {
-    console.log(this.props.air_data);
+    console.log(this.props.air_data.data);
 
     return (
       <div className={styles.HomeContainer}>
         <div>
           <Title><span role='img' aria-label='emoji-leaf'>🍃</span> Breathe Safely</Title>
-          <SearchInput />
+          {this.renderLoading()}
         </div>
       </div>
     );
